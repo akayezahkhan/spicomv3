0ce8b25efd9cb6120254ab739d94b9439d03a837
Ayezah Maryam <ayeza.maryam123@gmail.com>
2024-12-25 13:45:44 +0500

    5

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,8 +4,8 @@ import GoogleProvider from 'next-auth/providers/google'
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: "37999786419-8p06dgbvighns7p5ic833lfai920lohe.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
   ]
 })
